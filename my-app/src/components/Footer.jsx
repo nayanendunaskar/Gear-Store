b8222e0261b16e8948df29cce713cbd9c3b6ccd7
nayanendu naskar <nayan19981@gmail.com>
2023-05-25 22:29:56 +0530

    footer component added

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -1,49 +1,60 @@
 import "./Footer.css";
 
-export const Footer = () => {
-  return (
-    <>
-      <footer className="footer">
-        <div className="social-links">
-          <a
-            className="social-pill"
-            href="https://github.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fa-brands fa-github"></i>
-          </a>
-          <a
-            className="social-pill"
-            href="https://twitter.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fa-brands fa-twitter"></i>
-          </a>
-          <a
-            className="social-pill"
-            href="https://www.linkedin.com/in/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fa-brands fa-linkedin"></i>
-          </a>
-        </div>
-        <div className="copyright-text">
-          <span>
-            Made with{" "}
-            <span role="img" aria-label="red-heart">
-              ❤️
-            </span>{" "}
-            by{" "}
-            <span style={{ fontWeight: "600", margin: "0px 6px 0px 0px" }}>
-              Nayanendu Naskar
-            </span>
-          </span>
-          &copy; {new Date().getFullYear()} GearStore
-        </div>
+export function Footer(){
+  return(
+      <div>
+           <footer>
+          <div className="footer">
+
+              <div className="help-section">
+                  <div className="section-header"><strong>Help</strong></div>
+                  
+                  <div>Contact us</div>
+                  <div>FAQ</div>
+                  <div>Size guide</div>
+                  <div>Sale Exclusions</div>
+
+              </div>
+
+              <div className="help-section">
+                  <div className="section-header"><strong>Help</strong></div>
+                 
+                  <div>Contact us</div>
+                  <div>FAQ</div>
+                  <div>Size guide</div>
+                  <div>Sale Exclusions</div>
+              </div>
+
+              <div className="help-section">
+                  <div className="section-header"><strong>Help</strong></div>
+                  
+                  <div>Contact us</div>
+                  <div>FAQ</div>
+                  <div>Size guide</div>
+                  <div>Sale Exclusions</div>
+              </div>
+
+              <div className="help-section">
+                  <div className="section-header"><strong>Help</strong></div>
+                  <div>Contact us</div>
+                  <div>FAQ</div>
+                  <div>Size guide</div>
+                  <div>Sale Exclusions</div>
+              </div> 
+
+              <div className="about-us-section">
+                  <div className="our-logo">GearStore</div>
+                  <p className="about-us-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, dolorem!</p>
+                  <div className="subFooterContainer">
+                      <div className="footerPills fab fa-twitter"></div>
+                      <div className="footerPills fab fa-linkedin"></div>
+                      <div className="footerPills fab fa-github"></div>
+                  </div>
+              </div>
+
+          </div>
       </footer>
-    </>
-  );
-};
+
+      </div>
+  )
+}
