c86c433c6dacac37d1e60a0f0ec0e394b2e2c54f
nayanendu naskar <nayan19981@gmail.com>
2023-06-05 22:37:28 +0530

    footer modified

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import "./Footer.css";
+import {FaGithub, FaLinkedin, FaTwitter} from "react-icons/fa";
 
 export function Footer(){
   return(
@@ -44,11 +45,32 @@ export function Footer(){
 
               <div className="about-us-section">
                   <div className="our-logo">GearStore</div>
-                  <p className="about-us-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, dolorem!</p>
+                  <p className="about-us-content">Made with ❤️ by <strong>Nayanendu Naskar</strong> @{new Date().getFullYear()}</p>
                   <div className="subFooterContainer">
-                      <div className="footerPills fab fa-twitter"></div>
-                      <div className="footerPills fab fa-linkedin"></div>
-                      <div className="footerPills fab fa-github"></div>
+
+                  <div>
+                    <a
+                      href="https://twitter.com/nayanendunaskar"
+                      target="_blank">
+                      <FaTwitter className="footerPills fab fa-twitter" />
+                    </a>
+                    </div>
+
+                      <div>
+                    <a
+                      href="https://www.linkedin.com/in/nayanendu-naskar-802344245/"
+                      target="_blank">
+                      <FaLinkedin className="footerPills fab fa-linkedin" />
+                    </a>
+                    </div>
+
+                      <div>
+                    <a
+                      href="https://github.com/nayanendunaskar"
+                      target="_blank">
+                      <FaGithub className="footerPills fab fa-github" />
+                    </a>
+                    </div>
                   </div>
               </div>
 
